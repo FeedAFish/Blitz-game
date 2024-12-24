850bbe1a922f6b2180d87d5820175daadcd57aa0
FeedAFish <phamtuankietdn2000@gmail.com>
2024-12-24 19:20:06 +0100

    add downloading data for updating

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from utils import main_game
 
-game = main_game.Game(800, 600)
-game.run()
+# Run the download file
+from utils import data_dl
+
+data_dl.Download_UI(
+    version="0.2",
+    url="https://drive.google.com/uc?id=1_e2zIMJjbTr-qf5qGEiFO4UQTw8pqBTF",
+)
+
+# Run the main game
+try:
+    game = main_game.Game(800, 600)
+    game.run()
+except Exception as e:
+    with open("error.log", "a") as f:
+        f.writelines(str(e))
